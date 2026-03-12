f0507794245c6fb4c8f094a753f99204ec249887
Cantnot030 <masaphantom@gmail.com>
2026-03-13 08:05:35 +0900

    section-prop.js を更新

--- js/section-prop.js
+++ js/section-prop.js
@@ -105,4 +105,96 @@ document.getElementById("result").innerHTML =
 
 }
 
+function ibeamCalc(){
+
+let B1 = parseFloat(document.getElementById("B1").value);
+let B2 = parseFloat(document.getElementById("B2").value);
+let H = parseFloat(document.getElementById("H").value);
+let tf = parseFloat(document.getElementById("tf").value);
+let tw = parseFloat(document.getElementById("tw").value);
+
+let A = B1*tf + B2*tf + (H-2*tf)*tw;
+
+let Ix =
+(B1*Math.pow(tf,3))/12 +
+(B1*tf)*Math.pow((H/2 - tf/2),2) +
+
+(B2*Math.pow(tf,3))/12 +
+(B2*tf)*Math.pow((H/2 - tf/2),2) +
+
+(tw*Math.pow((H-2*tf),3))/12;
+
+let Zx = Ix/(H/2);
+
+document.getElementById("result").innerHTML =
+"A = "+A+"<br>"+
+"Ix = "+Ix+"<br>"+
+"Zx = "+Zx;
+
+}
+
+function tbeamCalc(){
+
+let B = parseFloat(document.getElementById("B").value);
+let H = parseFloat(document.getElementById("H").value);
+let tf = parseFloat(document.getElementById("tf").value);
+let tw = parseFloat(document.getElementById("tw").value);
+
+let A1 = B*tf;
+let A2 = tw*(H-tf);
+
+let y1 = H - tf/2;
+let y2 = (H-tf)/2;
+
+let A = A1 + A2;
+
+let ybar = (A1*y1 + A2*y2)/A;
+
+let Ix1 = (B*Math.pow(tf,3))/12 + A1*Math.pow(y1-ybar,2);
+let Ix2 = (tw*Math.pow((H-tf),3))/12 + A2*Math.pow(y2-ybar,2);
+
+let Ix = Ix1 + Ix2;
+
+let Zx = Ix/(H-ybar);
+
+document.getElementById("result").innerHTML =
+"A = "+A+"<br>"+
+"Ix = "+Ix+"<br>"+
+"Zx = "+Zx;
+
+}
+
+function angleCalc(){
+
+let B = parseFloat(document.getElementById("B").value);
+let H = parseFloat(document.getElementById("H").value);
+let t = parseFloat(document.getElementById("t").value);
+
+let A1 = B*t;
+let A2 = H*t;
+let A3 = t*t;
+
+let A = A1 + A2 - A3;
+
+let y1 = t/2;
+let y2 = H/2;
+let y3 = t/2;
+
+let ybar = (A1*y1 + A2*y2 - A3*y3)/A;
+
+let Ix1 = (B*Math.pow(t,3))/12 + A1*Math.pow(y1-ybar,2);
+let Ix2 = (t*Math.pow(H,3))/12 + A2*Math.pow(y2-ybar,2);
+let Ix3 = (t*Math.pow(t,3))/12 + A3*Math.pow(y3-ybar,2);
+
+let Ix = Ix1 + Ix2 - Ix3;
+
+let Zx = Ix/(H-ybar);
+
+document.getElementById("result").innerHTML =
+"A = "+A+"<br>"+
+"Ix = "+Ix+"<br>"+
+"Zx = "+Zx;
+
+}
+
 }
