1a59b89da033a999cb7a59a6efcca757e9c75cc5
Cantnot030 <masaphantom@gmail.com>
2026-03-31 12:49:08 +0900

    section-prop.js を更新

--- js/section-prop.js
+++ js/section-prop.js
@@ -8,6 +8,8 @@ document.getElementById("circleInputs").style.display = "none";
 document.getElementById("pipeInputs").style.display = "none";
 document.getElementById("hbeamInputs").style.display = "none";
 
+clearResult();
+
 if(shape==="rect"){
 document.getElementById("rectInputs").style.display = "block";
 drawRect()
@@ -299,3 +301,9 @@ function copyText(id) {
     .catch(() => alert("コピー失敗"));
 }
 */
+
+function clearResult(){
+  document.getElementById("A_val").textContent = "";
+  document.getElementById("Ix_val").textContent = "";
+  document.getElementById("Zx_val").textContent = "";
+}
