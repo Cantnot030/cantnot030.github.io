1f4b5d95c4e385534c4c77dc4f6a9cfa10f9c915
Cantnot030 <masaphantom@gmail.com>
2026-04-08 22:36:08 +0900

    Update setupInteraction.js

--- ja/cae/shell/js/setupInteraction.js
+++ ja/cae/shell/js/setupInteraction.js
@@ -14,130 +14,129 @@ function setupInteraction(){
     lastY = e.offsetY;
   });
 
-canvas.addEventListener("mousemove", e => {
-  if(!isDragging) return;
+  canvas.addEventListener("mousemove", e => {
+    if(!isDragging) return;
 
-  let dx = e.offsetX - lastX;
-  let dy = - ( e.offsetY - lastY );
-
-  view.offsetX += dx;
-  view.offsetY += dy;
-
-  lastX = e.offsetX;
-  lastY = e.offsetY;
-
-  drawCanvas(nodes, elems);
-});
-
-canvas.addEventListener("mouseup", () => {
-  isDragging = false;
-});
-
-canvas.addEventListener("mouseleave", () => {
-  isDragging = false;
-});
-
-//スマホスワイプ
-let lastTouchX = 0;
-let lastTouchY = 0;
-
-canvas.addEventListener("touchstart", e => {
-  if(e.touches.length === 1){
-    lastTouchX = e.touches[0].clientX;
-    lastTouchY = e.touches[0].clientY;
-  }
-});
-
-canvas.addEventListener("touchmove", e => {
-
-  if(e.touches.length === 1){
-
-    e.preventDefault();
-
-    let x = e.touches[0].clientX;
-    let y = e.touches[0].clientY;
-
-    let dx = x - lastTouchX;
-    let dy = - ( y - lastTouchY );
+    let dx = e.offsetX - lastX;
+    let dy = - ( e.offsetY - lastY );
 
     view.offsetX += dx;
     view.offsetY += dy;
 
-    lastTouchX = x;
-    lastTouchY = y;
+    lastX = e.offsetX;
+    lastY = e.offsetY;
 
     drawCanvas(nodes, elems);
-  }
+  });
 
-}, { passive: false });
+  canvas.addEventListener("mouseup", () => {
+    isDragging = false;
+  });
 
+  canvas.addEventListener("mouseleave", () => {
+    isDragging = false;
+  });
 
-// PCホイール
-canvas.addEventListener("wheel", function(e){
-  e.preventDefault();
+  //スマホスワイプ
+  let lastTouchX = 0;
+  let lastTouchY = 0;
 
-  const zoom = 1.1;
+  canvas.addEventListener("touchstart", e => {
+    if(e.touches.length === 1){
+      lastTouchX = e.touches[0].clientX;
+      lastTouchY = e.touches[0].clientY;
+    }
+  });
 
-  const rect = canvas.getBoundingClientRect();
+  canvas.addEventListener("touchmove", e => {
 
-  // キャンバス中心
-  const cx = canvas.width / 2;
-  const cy = canvas.height / 2;
+    if(e.touches.length === 1){
+      e.preventDefault();
 
-  let prevScale = view.scale;
+      let x = e.touches[0].clientX;
+      let y = e.touches[0].clientY;
 
-  if(e.deltaY < 0){
-    view.scale *= zoom;
-  }else{
-    view.scale /= zoom;
-  }
+      let dx = x - lastTouchX;
+      let dy = - ( y - lastTouchY );
 
-  let scaleRatio = view.scale / prevScale;
+      view.offsetX += dx;
+      view.offsetY += dy;
 
-  // 中心を基準に補正
-  view.offsetX = cx - (cx - view.offsetX) * scaleRatio;
-  view.offsetY = cy - (cy - view.offsetY) * scaleRatio;
-
-  drawCanvas(nodes, elems);
-
-}, { passive: false });
-
-
-//スマホズーム
-canvas.addEventListener("touchmove", function(e){
-
-  if(e.touches.length === 2){
-
-    e.preventDefault();
-
-    let dx = e.touches[0].clientX - e.touches[1].clientX;
-    let dy = e.touches[0].clientY - e.touches[1].clientY;
-
-    let dist = Math.sqrt(dx*dx + dy*dy);
-
-    if(lastDist !== null){
-
-      let prevScale = view.scale;
-      let scaleChange = dist / lastDist;
-
-      view.scale *= scaleChange;
-
-      let scaleRatio = view.scale / prevScale;
-
-      // 中心基準
-      const cx = canvas.width / 2;
-      const cy = canvas.height / 2;
-
-      view.offsetX = cx - (cx - view.offsetX) * scaleRatio;
-      view.offsetY = cy - (cy - view.offsetY) * scaleRatio;
+      lastTouchX = x;
+      lastTouchY = y;
 
       drawCanvas(nodes, elems);
     }
 
-    lastDist = dist;
-  }
+  }, { passive: false });
 
-}, { passive: false });
+
+  // PCホイール
+  canvas.addEventListener("wheel", function(e){
+    e.preventDefault();
+
+    const zoom = 1.1;
+
+    const rect = canvas.getBoundingClientRect();
+
+    // キャンバス中心
+    const cx = canvas.width / 2;
+    const cy = canvas.height / 2;
+
+    let prevScale = view.scale;
+
+    if(e.deltaY < 0){
+      view.scale *= zoom;
+    }else{
+      view.scale /= zoom;
+    }
+
+    let scaleRatio = view.scale / prevScale;
+
+    // 中心を基準に補正
+    view.offsetX = cx - (cx - view.offsetX) * scaleRatio;
+    view.offsetY = cy - (cy - view.offsetY) * scaleRatio;
+
+    drawCanvas(nodes, elems);
+
+  }, { passive: false });
+
+
+  //スマホズーム
+  canvas.addEventListener("touchmove", function(e){
+
+    if(e.touches.length === 2){
+
+      e.preventDefault();
+
+      let dx = e.touches[0].clientX - e.touches[1].clientX;
+      let dy = e.touches[0].clientY - e.touches[1].clientY;
+
+      let dist = Math.sqrt(dx*dx + dy*dy);
+
+      if(lastDist !== null){
+
+        let prevScale = view.scale;
+        let scaleChange = dist / lastDist;
+
+        view.scale *= scaleChange;
+
+        let scaleRatio = view.scale / prevScale;
+
+        // 中心基準
+        const cx = canvas.width / 2;
+        const cy = canvas.height / 2;
+
+        view.offsetX = cx - (cx - view.offsetX) * scaleRatio;
+        view.offsetY = cy - (cy - view.offsetY) * scaleRatio;
+
+        drawCanvas(nodes, elems);
+      }
+
+      lastDist = dist;
+    }
+
+  }, { passive: false });
 
   canvas.addEventListener("touchend", function(){
     lastDist = null;
@@ -146,28 +145,24 @@ canvas.addEventListener("touchmove", function(e){
   //PC 要素クリック
   canvas.addEventListener("click", function(e){
 
-  let rect = canvas.getBoundingClientRect();
+    let rect = canvas.getBoundingClientRect();
 
-  let x = e.clientX - rect.left;
-  //let y = e.clientY - rect.top;
-　let y = canvas.height - (e.clientY - rect.top);
+    let x = e.clientX - rect.left;
+    //let y = e.clientY - rect.top;
+　  let y = canvas.height - (e.clientY - rect.top);
 
-  let elem = findElement(x, y);
+    let elem = findElement(x, y);
 
-  if(elem){
-    showElemInfo(elem);
-  }
+    if(elem){
+      showElemInfo(elem);
+    }
   
   });
-
-//-----------------------
 }
 //マウス操作、スマホタッチ設定終了
 
-
-
-  //三角形判定
-  function isInsideTriangle(px, py, p1, p2, p3){
+//三角形判定
+function isInsideTriangle(px, py, p1, p2, p3){
 
   function sign(a, b, c){
     return (a.x - c.x)*(b.y - c.y) - (b.x - c.x)*(a.y - c.y);
