fb9edb46193846dac9f2e6979e110f71421f1003
Cantnot030 <masaphantom@gmail.com>
2026-04-09 22:14:45 +0900

    calc-quality.js を更新

--- ja/cae/shell/js/calc-quality.js
+++ ja/cae/shell/js/calc-quality.js
@@ -1,3 +1,4 @@
+//面積
 function calcArea(pts){
 
   let area = 0;
@@ -11,3 +12,44 @@ function calcArea(pts){
 
   return Math.abs(area) / 2;
 }
+
+//辺長算定
+function calcEdges(pts){
+
+  let edges = [];
+
+  for(let i=0; i<pts.length; i++){
+
+    let p1 = pts[i];
+    let p2 = pts[(i+1) % pts.length]; // ←最後は最初に戻る
+
+    let dx = p2.x - p1.x;
+    let dy = p2.y - p1.y;
+
+    let length = Math.sqrt(dx*dx + dy*dy);
+
+    edges.push(length);
+  }
+
+  return edges;
+}
+
+//最短辺
+function calcMinEdge(pts){
+
+  let edges = calcEdges(pts);
+  return Math.min(...edges);
+
+}
+
+//最長辺
+function calcMaxEdge(pts){
+
+  let edges = calcEdges(pts);
+  return Math.max(...edges);
+
+}
+
+
+
+
